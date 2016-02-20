bfa290d4df2a7a2fbc5336fa8a623af5bf615230
Felipe Sikansi <felipe.sikansi@gmail.com>
2016-02-20 17:37:54 -0200

    First support to GPIO 7 segment display tested

--- GPIOController.py
+++ GPIOController.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 
-class InputTypes(Enum):
+class GPIOController:
+
     RPM_1_LEVEL = 17
     RPM_2_LEVEL = 18
     RPM_3_LEVEL = 27
@@ -14,44 +15,40 @@ class InputTypes(Enum):
     SEG7_F = 21
     SEG7_G = 20
 
-class GPIOController:
-
     numbers = {
-        0:(1,1,1,1,1,1,0),
-        1:(0,1,1,0,0,0,0),
-        2:(1,1,0,1,1,0,1),
-        3:(1,1,1,1,0,0,1),
-        4:(0,1,1,0,0,1,1),
-        5:(1,0,1,1,0,1,1),
-        6:(1,0,1,1,1,1,1),
-        7:(1,1,1,0,0,0,0),
-        8:(1,1,1,1,1,1,1),
-        9:(1,1,1,1,0,1,1)}
-
-    segments = [InputTypes.SEG7_A, InputTypes.SEG7_B, InputTypes.SEG7_C, InputTypes.SEG7_D, InputTypes.SEG7_E, InputTypes.SEG7_F, InputTypes.SEG7_G]
+        0:(0,0,0,0,0,0,1),
+        1:(1,0,0,1,1,1,1),
+        2:(0,0,1,0,0,1,0),
+        3:(0,0,0,0,1,1,0),
+        4:(1,0,0,1,1,0,0),
+        5:(0,1,0,0,1,0,0),
+        6:(0,1,0,0,0,0,0),
+        7:(0,0,0,1,1,1,1),
+        8:(0,0,0,0,0,0,0),
+        9:(0,0,0,0,1,0,0)}
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         #configure_leds()
-        configure_7_segments()
+        self.configure_7_segments()
 
-    def configure_leds():
-        GPIO.setup(InputTypes.RPM_1_LEVEL,GPIO.OUT)
-        GPIO.setup(InputTypes.RPM_2_LEVEL,GPIO.OUT)
-        GPIO.setup(InputTypes.RPM_3_LEVEL,GPIO.OUT)
-        GPIO.setup(InputTypes.RPM_4_LEVEL,GPIO.OUT)
-        GPIO.setup(InputTypes.RPM_5_LEVEL,GPIO.OUT)
+    def configure_leds(self):
+        GPIO.setup(RPM_1_LEVEL,GPIO.OUT)
+        GPIO.setup(RPM_2_LEVEL,GPIO.OUT)
+        GPIO.setup(RPM_3_LEVEL,GPIO.OUT)
+        GPIO.setup(RPM_4_LEVEL,GPIO.OUT)
+        GPIO.setup(RPM_5_LEVEL,GPIO.OUT)
 
-    def configure_7_segments():
-        GPIO.setup(InputTypes.SEG7_A,GPIO.OUT)
-        GPIO.setup(InputTypes.SEG7_B,GPIO.OUT)
-        GPIO.setup(InputTypes.SEG7_C,GPIO.OUT)
-        GPIO.setup(InputTypes.SEG7_D,GPIO.OUT)
-        GPIO.setup(InputTypes.SEG7_E,GPIO.OUT)
-        GPIO.setup(InputTypes.SEG7_F,GPIO.OUT)
-        GPIO.setup(InputTypes.SEG7_G,GPIO.OUT)
+    def configure_7_segments(self):
+        GPIO.setup(self.SEG7_A,GPIO.OUT, initial=1)
+        GPIO.setup(self.SEG7_B,GPIO.OUT, initial=1)
+        GPIO.setup(self.SEG7_C,GPIO.OUT, initial=1)
+        GPIO.setup(self.SEG7_D,GPIO.OUT, initial=1)
+        GPIO.setup(self.SEG7_E,GPIO.OUT, initial=1)
+        GPIO.setup(self.SEG7_F,GPIO.OUT, initial=1)
+        GPIO.setup(self.SEG7_G,GPIO.OUT, initial=1)
 
-    def update_leds(telemetry_data):
+    def update_leds(self, telemetry_data):
         rpm_percentage = int((telemetry_data.rpm / telemetry_data.max_rpm) * 100)
         if (rpm_percentage > 95):
             GPIO.output(InputTypes.RPM_1_LEVEL,GPIO.HIGH)
@@ -90,10 +87,16 @@ class GPIOController:
             GPIO.output(InputTypes.RPM_4_LEVEL,GPIO.LOW)
             GPIO.output(InputTypes.RPM_5_LEVEL,GPIO.LOW)
 
-    def update_7_segments(telemetry_data):
+    def update_7_segments(self, telemetry_data):
         if (telemetry_data.current_gear < 10):
-            GPIO.output(segments, numbers(telemetry_data.current_gear))
+            GPIO.output(self.SEG7_A, self.numbers[telemetry_data.current_gear][0])
+            GPIO.output(self.SEG7_B, self.numbers[telemetry_data.current_gear][1])
+            GPIO.output(self.SEG7_C, self.numbers[telemetry_data.current_gear][2])
+            GPIO.output(self.SEG7_D, self.numbers[telemetry_data.current_gear][3])
+            GPIO.output(self.SEG7_E, self.numbers[telemetry_data.current_gear][4])
+            GPIO.output(self.SEG7_F, self.numbers[telemetry_data.current_gear][5])
+            GPIO.output(self.SEG7_G, self.numbers[telemetry_data.current_gear][6])
 
-    def update_all(telemetry_data):
-        update_7_segments(telemetry_data)
+    def update_all(self, telemetry_data):
+        self.update_7_segments(telemetry_data)
         #update_leds(telemetry_data)
