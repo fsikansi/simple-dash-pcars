2ed5dca891d57cdf905526e1354f66bbe74b227d
Felipe Sikansi <felipe.sikansi@gmail.com>
2016-02-26 19:33:20 -0300

    Added support to LCD screen - not tested yet

--- GPIOController.py
+++ GPIOController.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import Adafruit_CharLCD
 
 class GPIOController:
 
@@ -14,6 +15,12 @@ class GPIOController:
     SEG7_E = 13
     SEG7_F = 21
     SEG7_G = 20
+    LCD_RS = 4
+    LCD_EN = 10
+    LCD_DATA_1 = 19
+    LCD_DATA_2 = 26
+    LCD_DATA_3 = 24
+    LCD_DATA_4 = 12
 
     numbers = {
         0:(0,0,0,0,0,0,1),
@@ -29,8 +36,10 @@ class GPIOController:
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
-        #configure_leds()
+        #self.configure_leds()
         self.configure_7_segments()
+        self.configure_lcd()
+        self.lcd = None
 
     def configure_leds(self):
         GPIO.setup(RPM_1_LEVEL,GPIO.OUT)
@@ -48,44 +57,48 @@ class GPIOController:
         GPIO.setup(self.SEG7_F,GPIO.OUT, initial=1)
         GPIO.setup(self.SEG7_G,GPIO.OUT, initial=1)
 
+    def configure_lcd(self):
+        self.lcd = Adafruit_CharLCD(LCD_RS, LCD_EN, [LCD_DATA_1, LCD_DATA_2, LCD_DATA_3, LCD_DATA_4])
+
+
     def update_leds(self, telemetry_data):
         rpm_percentage = int((telemetry_data.rpm / telemetry_data.max_rpm) * 100)
         if (rpm_percentage > 95):
-            GPIO.output(InputTypes.RPM_1_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_2_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_3_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_4_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_5_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_1_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_2_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_3_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_4_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_5_LEVEL,GPIO.HIGH)
         elif (rpm_percentage > 91):
-            GPIO.output(InputTypes.RPM_1_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_2_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_3_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_4_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_5_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_1_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_2_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_3_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_4_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_5_LEVEL,GPIO.LOW)
         elif (rpm_percentage > 87):
-            GPIO.output(InputTypes.RPM_1_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_2_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_3_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_4_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_5_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_1_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_2_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_3_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_4_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_5_LEVEL,GPIO.LOW)
         elif (rpm_percentage > 84):
-            GPIO.output(InputTypes.RPM_1_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_2_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_3_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_4_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_5_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_1_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_2_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_3_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_4_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_5_LEVEL,GPIO.LOW)
         elif (rpm_percentage > 80):
-            GPIO.output(InputTypes.RPM_1_LEVEL,GPIO.HIGH)
-            GPIO.output(InputTypes.RPM_2_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_3_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_4_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_5_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_1_LEVEL,GPIO.HIGH)
+            GPIO.output(self.RPM_2_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_3_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_4_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_5_LEVEL,GPIO.LOW)
         else:
-            GPIO.output(InputTypes.RPM_1_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_2_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_3_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_4_LEVEL,GPIO.LOW)
-            GPIO.output(InputTypes.RPM_5_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_1_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_2_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_3_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_4_LEVEL,GPIO.LOW)
+            GPIO.output(self.RPM_5_LEVEL,GPIO.LOW)
 
     def update_7_segments(self, telemetry_data):
         if (telemetry_data.current_gear < 10):
@@ -97,6 +110,12 @@ class GPIOController:
             GPIO.output(self.SEG7_F, self.numbers[telemetry_data.current_gear][5])
             GPIO.output(self.SEG7_G, self.numbers[telemetry_data.current_gear][6])
 
+    def update_lcd(self, telemetry_data):
+        self.lcd.clear()
+        self.lcd.message(str(int(Convert.speed_to_kph(telemetry_data.speed))) + " KPH")
+
+
     def update_all(self, telemetry_data):
         self.update_7_segments(telemetry_data)
-        #update_leds(telemetry_data)
+        self.update_leds(telemetry_data)
+        self.update_lcd(telemetry_data)
